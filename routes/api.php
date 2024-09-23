c338eb2f19b386a81921be152a2f64f297fb0936
Rayard <rayard.sookoo@gov.tt>
2024-09-23 18:39:49 -0400

    first commit

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return 'Hello World!';
+});
+
+// Auth Routes
+Route::post('/register', [Auth::class, 'register']);
+Route::post('/login', [Auth::class, 'login']);
+Route::post('/logout', [Auth::class, 'logout'])->middleware('auth:sanctum');
