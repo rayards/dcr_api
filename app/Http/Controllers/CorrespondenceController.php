54840fd05db223cf2d94e23a76513620d48f311c
Rayard <rayard_1@hotmail.com>
2025-02-12 13:02:57 -0400

    add Correspondence resource with controller, requests, model, policy, and migration

---
+++ app/Http/Controllers/CorrespondenceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCorrespondenceRequest;
+use App\Http\Requests\UpdateCorrespondenceRequest;
+use App\Models\Correspondence;
+
+class CorrespondenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCorrespondenceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Correspondence $correspondence)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Correspondence $correspondence)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCorrespondenceRequest $request, Correspondence $correspondence)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Correspondence $correspondence)
+    {
+        //
+    }
+}
