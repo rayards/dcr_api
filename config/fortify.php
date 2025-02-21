c3585a069d591dd0d8a01cc049a74550c894164d
Rayard <rayard_1@hotmail.com>
2025-02-21 14:13:05 -0400

    update Fortify configuration; change home redirect path to root and disable views, comment out unused features

--- config/fortify.php
+++ config/fortify.php
@@ -73,7 +73,7 @@ return [
     |
     */
 
-    'home' => '/home',
+    'home' => '/',
 
     /*
     |--------------------------------------------------------------------------
@@ -130,7 +130,7 @@ return [
     |
     */
 
-    'views' => true,
+    'views' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -145,15 +145,15 @@ return [
 
     'features' => [
         Features::registration(),
-        Features::resetPasswords(),
+        // Features::resetPasswords(),
         // Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0,
-        ]),
+        // Features::updateProfileInformation(),
+        // Features::updatePasswords(),
+        // Features::twoFactorAuthentication([
+        // 'confirm' => true,
+        // 'confirmPassword' => true,
+        // 'window' => 0,
+        // ]),
     ],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
-use App\Http\Controllers\Auth;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FilingAreaController;
 use App\Http\Controllers\CorrespondenceController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
+Route::get('/user', function (Request $request) {
+    return $request->user();
+});
 // })->middleware('auth:sanctum');
 
 Route::get('/', function () {
     return 'Hello World!';
 });
-
-// Route::get('/user', [Auth::class, 'user']);
 
 //Correspondence Routes
 Route::apiResource('/correspondence', CorrespondenceController::class);
